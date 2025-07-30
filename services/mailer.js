870d817727697e453280ba4addf7a4adf6a8ec6d
Ailén <ailenechaguibel@gmail.com>
2025-07-30 10:10:33 +0200

    cambio email para enviar el token

--- services/mailer.js
+++ services/mailer.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function enviarEmail(to, subject, html) {
     try {
      const { data, error } = await resend.emails.send({
-         from:'onboarding@resend.dev',
+         from: 'no-reply@resend.dev',
          to,
          subject,
          html
